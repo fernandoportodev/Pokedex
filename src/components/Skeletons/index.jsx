dfcefa8ae17169395c22672694346f6a1876a6ab
Fernando Porto <fernandogomes143@gmail.com>
2023-03-18 23:27:13 -0300

    [@fernandoportodev] Created application Pokedex with Axios(API Pokédex) and Material UI

---
+++ src/components/Skeletons/index.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Skeleton } from "@mui/material";
+import { Container } from "@mui/system";
+
+export const Skeletons = () => {
+  return (
+    <Container maxWidth="xxl">
+      <Skeleton
+        variant="rounded"
+        width="100%"
+        height={150}
+        sx={{ marginBottom: "1em" }}
+      />
+      <Skeleton
+        variant="rounded"
+        width="100%"
+        height={150}
+        sx={{ marginBottom: "1em" }}
+      />
+      <Skeleton
+        variant="rounded"
+        width="100%"
+        height={150}
+        sx={{ marginBottom: "1em" }}
+      />
+      <Skeleton
+        variant="rounded"
+        width="100%"
+        height={150}
+        sx={{ marginBottom: "1em" }}
+      />
+      <Skeleton
+        variant="rounded"
+        width="100%"
+        height={150}
+        sx={{ marginBottom: "1em" }}
+      />
+    </Container>
+  );
+};
---
+++ src/pages/Home.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import { Container } from "@mui/system";
+import { Grid } from "@mui/material";
+import Navbar from "../components/Navbar";
+import PokemonCard from "../components/PokemonCard";
+import axios from "axios";
+import { Skeletons } from "../components/Skeletons";
+
+export const Home = () => {
+  const [pokemons, setPokemons] = useState([]);
+  useEffect(() => {
+    getPokemons();
+  }, []);
+
+  const getPokemons = () => {
+    let endPoints = [];
+    for (let i = 1; i <= 200; i++) {
+      endPoints.push(`https://pokeapi.co/api/v2/pokemon/${i}`);
+    }
+    axios
+      .all(endPoints.map((endPoint) => axios.get(endPoint)))
+      .then((res) => setPokemons(res));
+  };
+
+  const pokemonFilter = (name) => {
+    let filteredPokemons = [];
+    if (name === "") {
+      getPokemons();
+    }
+    for (let i in pokemons) {
+      if (pokemons[i].data.name.includes(name)) {
+        filteredPokemons.push(pokemons[i]);
+      }
+    }
+    setPokemons(filteredPokemons);
+  };
+
+  return (
+    <div>
+      <Navbar pokemonFilter={pokemonFilter} />
+      <Container maxWidth="false">
+        <Grid container spacing={3}>
+          {pokemons.length === 0 ? (
+            <Skeletons />
+          ) : (
+            pokemons.map((pokemon, key) => (
+              <Grid item xs={12} sm={6} md={4} lg={2} key={key}>
+                <PokemonCard
+                  name={pokemon.data.name}
+                  image={pokemon.data.sprites.front_default}
+                  types={pokemon.data.types}
+                />
+              </Grid>
+            ))
+          )}
+        </Grid>
+      </Container>
+    </div>
+  );
+};
